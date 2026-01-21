1dd21b15abae0ff9e0b611c61c0df682ec844533
Joe <joe@phoenixfire.com.au>
2026-01-21 16:41:03 +1100

    Add property tabs for quotations and routines

--- properties/urls.py
+++ properties/urls.py
@@ -6,6 +6,8 @@ from .views import (
     PropertyDetailView,
     PropertyListView,
     PropertyUpdateView,
+    PropertyQuotationsView,
+    PropertyRoutinesView,
 )
 
 app_name = "properties"
@@ -13,7 +15,11 @@ app_name = "properties"
 urlpatterns = [
     path("", PropertyListView.as_view(), name="list"),
     path("new/", PropertyCreateView.as_view(), name="create"),
+
     path("<int:pk>/", PropertyDetailView.as_view(), name="detail"),
+    path("<int:pk>/quotations/", PropertyQuotationsView.as_view(), name="quotations"),
+    path("<int:pk>/routines/", PropertyRoutinesView.as_view(), name="routines"),
+
     path("<int:pk>/edit/", PropertyUpdateView.as_view(), name="update"),
     path("<int:pk>/delete/", PropertyDeleteView.as_view(), name="delete"),
 ]
--- properties/views.py
+++ properties/views.py
@@ -11,6 +11,11 @@ from django.urls import reverse_lazy
 from .models import Property
 from .forms import PropertyForm
 
+# These imports assume your models are named like this.
+# If your model names differ, tell me and I'll adjust.
+from quotations.models import Quotation
+from routines.models import Routine
+
 
 class PropertyListView(ListView):
     model = Property
@@ -41,6 +46,34 @@ class PropertyListView(ListView):
 class PropertyDetailView(DetailView):
     model = Property
     template_name = "properties/property_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Used by your tabs in the template
+        context["tab"] = "details"
+        return context
+
+
+class PropertyQuotationsView(DetailView):
+    model = Property
+    template_name = "properties/property_quotations.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tab"] = "quotations"
+        context["quotations"] = Quotation.objects.filter(property=self.object).order_by("-id")
+        return context
+
+
+class PropertyRoutinesView(DetailView):
+    model = Property
+    template_name = "properties/property_routines.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tab"] = "routines"
+        context["routines"] = Routine.objects.filter(property=self.object).order_by("-id")
+        return context
 
 
 class PropertyCreateView(CreateView):
