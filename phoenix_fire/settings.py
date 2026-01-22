2026e41bc13d92e27b06d2b82561ca1dc3628c08
Joe <joe@phoenixfire.com.au>
2026-01-22 23:25:15 +1100

    Log Django request exceptions to console

--- phoenix_fire/settings.py
+++ phoenix_fire/settings.py
@@ -146,3 +146,19 @@ MS_REDIRECT_URI = os.environ.get(
 )
 
 MS_GRAPH_SCOPES = ["User.Read", "Mail.Send"]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "root": {"handlers": ["console"], "level": "INFO"},
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
